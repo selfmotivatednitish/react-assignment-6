9c9de15ebfa5af7f35a15743c80e89e544764f88
Nitish Kumar <myhobbyissolvemath@gmail.com>
2023-04-11 10:25:36 +0530

    backgroundcolor fixed

--- src/components/ThemeChanger.js
+++ src/components/ThemeChanger.js
@@ -6,11 +6,12 @@ class ThemeChanger extends Component {
         const { theme, setTheme } = this.props;
 
         return (
-            <div style={{backgroundColor: localStorage.getItem('theme') === 'light' ? 'white' : 'black', height: 100 + "vh", width: 200 + "vh"}}>
+            <div style={{ backgroundColor: localStorage.getItem('theme') === 'light' ? 'white' : 'black', height: window.innerHeight + "px", width: window.innerWidth + "px", alignItems: 'center' }}>
                 <button onClick={() => setTheme('light')}>Light Theme</button>
-                <button onClick={() => setTheme('dark')} style={{backgroundColor: 'black', color: 'white'
-            }}>Dark Theme</button>
-                <p style={{color: localStorage.getItem('theme') === 'dark' ? 'white' : 'black'}}>Current Theme: {theme}</p>
+                <button onClick={() => setTheme('dark')} style={{
+                    backgroundColor: 'black', color: 'white'
+                }}>Dark Theme</button>
+                <p style={{ color: localStorage.getItem('theme') === 'dark' ? 'white' : 'black' }}>Current Theme: {theme}</p>
             </div>
         );
     }
